a4b7290e71ff28fd77bdfa917bea0b77013c779f
Jon Carstens <jjcarstens@me.com>
2021-04-26 18:12:48 -0600

    add system env vars for release

--- config/releases.exs
+++ config/releases.exs
@@ -2,11 +2,20 @@ import Config
 
 config :nerves_tips, NervesTipsWeb.Endpoint,
   server: true,
-  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+  http: [
+    port: {:system, "PORT"}, # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+    transport_options: [socket_opts: [:inet6]]
+  ],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  live_view: [signing_salt: System.get_env("LIVE_VIEW_SIGNING_SALT")]
 
 config :nerves_tips, NervesTips.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   ssl: true,
-  pool_size: 9 # (25-6)/(1+1) size DB
+  pool_size: String.to_integer(System.get_env("POOL_SIZE"))
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: System.get_env("GITHUB_CLIENT_ID"),
+  client_secret: System.get_env("GITHUB_CLIENT_SECRET")
